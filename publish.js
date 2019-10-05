e3c732933333395f10b2067d230dac776a133194
Zaid <zaid.naom@gmail.com>
2019-10-05 16:56:10 +0200

    Updates

---
+++ publish.js
@@ -0,0 +1,15 @@
+var ghPages = require("gh-pages");
+
+var packageUrl = "https://github.com/Zaid-Ajaj/Feliz.git";
+
+console.log("Publishing to " + packageUrl);
+
+ghPages.publish("public", {
+    repo: packageUrl
+}, function (e) {
+    if (e === undefined) {
+        console.log("Finished publishing succesfully");
+    } else {
+        console.log("Error occured while publishing :(", e);
+    }
+});
